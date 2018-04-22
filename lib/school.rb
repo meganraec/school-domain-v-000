cc50d0db2750b754cfc1be5c2a2ab926604785fc
Megan Collier <meganraec@live.com>
2018-04-22 20:43:58 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+  attr_accessor :roster, :school_name
+  attr_reader :grade
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = Hash.new{|k, v| k[v] = []}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] << name
+  end
+
+  def grade(n)
+    @roster[n]
+  end
+
+  @school.sort ||= @school.sort_by {|grade, name| name}
+  
+end
